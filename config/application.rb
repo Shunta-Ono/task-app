f6ed74301a76b0f5bc8ae3b0982da34250f1645d
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-04-15 09:38:02 +0000

    devise導入

--- config/application.rb
+++ config/application.rb
@@ -15,16 +15,12 @@ module TaskApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs: false,
+            controller_specs: true
+    end
   end
 end
-
-config.generators do |g|
-      g.test_framework :rspec,
-                       fixtures: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: true,
-                       request_specs: false
-      g.fixture_replacement :factory_bot, dir: "spec/factories"
-    end
