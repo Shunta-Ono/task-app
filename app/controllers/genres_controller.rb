d8620ef0ea6d66ac12457c9824e0950aef8cc134
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-04-16 11:20:50 +0000

    [create]genre-function

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,12 +1,17 @@
 class GenresController < ApplicationController
 
   def new
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to genres_path
   end
 
   def index
+    @genres = Genre.all
   end
 
   def show
@@ -20,4 +25,11 @@ class GenresController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
